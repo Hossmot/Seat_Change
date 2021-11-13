025f5ff9fb886e597329ab227f3facaa5e3e621d
Hossmot <83452955+Hossmot@users.noreply.github.com>
2021-11-13 19:24:20 +0900

    Update Seat_Change.py

--- Seat_Change.py
+++ Seat_Change.py
@@ -10,6 +10,8 @@ HMSP_StudentSeatNum = []
 HMSP_StudentNumber = []
 
 
+
+
 def Show_CS():
     for v in range(5):
         for h in range(5):
@@ -70,15 +72,6 @@ def SetStudentSeat():
     Numinsert = int(0)
 
 
-def BasicSetting():
-    Class_Seat = [[1, 2, 3, 4, 5],
-                  [6, 7, 8, 9, 10],
-                  [11, 12, 13, 14, 15],
-                  [16, 17, 18, 19, 20],
-                  [21, 22, 23, 24, 25]]
-    CS_Storage = []
-    HMSP_StudentSeatNum = []
-    HMSP_StudentNumber = []
 
 
 def VerticalShuffle(j):
@@ -88,15 +81,15 @@ def VerticalShuffle(j):
     rd.shuffle(VerticalElementStorage)
     for a in range(5):
         Class_Seat[a][j-1] = VerticalElementStorage[a]
-        
-        
+
+
+
+
 def Main_1(i):
     if i == 1:
-        BasicSetting()
         Seat_Shuffle()
         Show_CS()
     elif i == 2:
-        BasicSetting()
         SetStudentSeat()
         Show_CS()
     elif i == 3:
@@ -120,11 +113,20 @@ def Main_2(i):
         exit()
 
 
+
 Engine = int(1)
 while (Engine == 1):
+
     Main_1(i=int(input("자리를 바꾸시려면 1, 고정 후 바꾸시려면 2를, 다음으로 넘어가시려면 3을 눌러주세요.")))
     Main_2(i=int(input("가로줄을 섞으시려면 1을, 세로줄을 섞으시려면 2를, 프로그램을 끝내시려면 3을 눌러주세요.")))
-
+    Class_Seat = [[1, 2, 3, 4, 5],
+                  [6, 7, 8, 9, 10],
+                  [11, 12, 13, 14, 15],
+                  [16, 17, 18, 19, 20],
+                  [21, 22, 23, 24, 25]]
+    CS_Storage = []
+    HMSP_StudentSeatNum = []
+    HMSP_StudentNumber = []
 
 
 
